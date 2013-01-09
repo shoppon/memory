fad62c708caba2b66557b51426bda746a1bc5f85
Peng Xiao <peng.xiao@myriadgroup.com>
2013-01-09 17:45:16 +0800

    add

--- memory/urls.py
+++ memory/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'memory.views.home', name='home'),
+     url(r'^$', 'upload.views.upload', name='upload'),
     # url(r'^memory/', include('memory.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
---
+++ upload/views.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def upload(request):
+    return HttpResponse("hello world")
