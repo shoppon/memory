ad12a4c78a1662a5d2f6e4bd6a211924e956f914
Peng Xiao <peng.xiao@myriadgroup.com>
2013-01-11 17:19:41 +0800

    update

--- memory/urls.py
+++ memory/urls.py
@@ -6,12 +6,12 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
+     url(r'^upload$', 'upload.views.upload', name='upload'),
      url(r'^$', 'upload.views.upload', name='upload'),
-    # url(r'^memory/', include('memory.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+#     url(r'^admin/', include(admin.site.urls)),
 )
--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,14 @@
+from _pyio import open
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def upload(request):
-    return HttpResponse("hello world")
+    if request.method == 'POST':
+        imagePath = '/m.jpg'
+        destination = open(imagePath, 'wb+')
+        print request.FILES['file_path']
+        for chunk in request.FILES['file_path'].chunks():
+            destination.write(chunk)
+        destination.close()
+        return HttpResponse("hello world")
